3aac17a578ab682268f710a8612e2afbe0ca8851
Francisco Encina <fcoencinaaba@gmail.com>
2023-06-08 14:28:45 -0400

    Etapa1 ok!

--- Etapa1/main.cpp
+++ Etapa1/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main(int argc, char * argv[]) {
     ifstream fin;
-    int nDoors, nWindows, x, y, direction_angle, zone;
+    int nDoors, nWindows;
     vector<Door *> doors;
     if (argc != 2) {
         cout << "Usage: "<<argv[0]<<" <configuration_file>" << endl;
@@ -20,18 +20,15 @@ int main(int argc, char * argv[]) {
         return -2;
     }
     cout << "Argument:" <<argv[1]<< endl;
-    string line;
-    getline(fin, line);
-    istringstream readConf(line);
 
-    readConf>>nDoors>>nWindows;
+    fin>>nDoors;
+    fin>>nWindows;
     cout<<"nDoors: "<<nDoors<<endl;
     cout<<"nWindows: "<<nWindows<<endl;
 
     for(int i=0; i<nDoors; i++) {
-        getline(fin, line);
-        istringstream readDoorConf(line);
-        readDoorConf>>x>>y>>direction_angle>>zone;
+        int x, y, direction_angle, zone;
+        fin>>x>>y>>direction_angle>>zone;
         cout<<"Puerta "<<i+1<<endl;
         doors.push_back(new Door(zone));
     }
@@ -41,5 +38,12 @@ int main(int argc, char * argv[]) {
         doors[i]->changeState();
         doors[i]->changeState();
     }
+    // Antes de cerrar el archivo y finalizar el programa
+    for (uint i = 0; i < doors.size(); i++) {
+        delete doors[i]; // Liberar la memoria asignada a cada objeto Door
+    }
+    // Limpiar el vector
+    doors.clear();
+
     fin.close();
 }
